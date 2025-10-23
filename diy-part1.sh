48db9a38aba99db72f7ff49c3dd193a617cf5443
725216 <djp.1@qq.com>
2025-10-23 14:49:10 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+#sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
+
+# homeproxy
+#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
