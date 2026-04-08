7d69602badf24e5926d7231728f1c79dc381b711
725216 <djp.1@qq.com>
2026-04-08 15:17:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
-# Add a feed source
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-#sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
-
-# homeproxy
-#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
-git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
-git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
 rm -rf feeds/luci/applications/luci-app-openclash
+
+git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan               #  定时任务设置
+
 # git clone --depth 1 https://github.com/coolsnowwolf/luci.git /tmp/tmp_luci
 # mv /tmp/tmp_luci/applications/luci-app-wireguard package/luci-app-wireguard/
 # rm -rf /tmp/tmp_luci
 
+# 添加常用插件源
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
