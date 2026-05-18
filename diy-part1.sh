39c213691daa86000311332ab47f01315dfe3209
725216 <djp.1@qq.com>
2026-05-19 07:57:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,8 @@ git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky           
 
 git clone https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan               #  定时任务设置
 
+git clone https://github.com/linkease/istore.git package/istore
+
 git clone --depth 1 https://github.com/jcorporation/myMPD.git /tmp/tmp_mympd
 mv /tmp/tmp_mympd/contrib/packaging/openwrt package/mympd/
 rm -rf /tmp/tmp_mympd
