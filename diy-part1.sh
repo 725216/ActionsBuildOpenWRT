af38d8213b7a3c9ef12470b04150042dc286e886
725216 <djp.1@qq.com>
2026-05-19 17:04:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,17 +3,10 @@
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-
-# 移除 openwrt feeds 过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
 # git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-
-# 移除过时的helloworld
-sed -i "/helloworld/d" "feeds.conf.default"
-./scripts/feeds clean
-sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-# 移除过时的openclash
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/helloworld
 rm -rf feeds/luci/applications/luci-app-openclash
 # git clone https://github.com/vernesong/OpenClash.git package/OpenC
 
@@ -22,6 +15,8 @@ git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky           
 git clone https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan               #  定时任务设置
 
 git clone https://github.com/linkease/istore.git package/istore
+
+git clone https://github.com/kenzok8/small.git package/small
 
 git clone --depth 1 https://github.com/jcorporation/myMPD.git /tmp/tmp_mympd
 mv /tmp/tmp_mympd/contrib/packaging/openwrt package/mympd/
