b89dbd811a08c129752bb46e5a1a9e8ada750773
725216 <djp.1@qq.com>
2026-07-02 09:26:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,7 @@
 # Modify default IP
 sed -i '/192.168.123.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-sudo apt install libfuse-dev
+sudo apt install libfuse-dev 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
