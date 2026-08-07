790c5a79feaeaba74742e03863feaf281abe59f1
725216 <djp.1@qq.com>
2026-08-07 14:00:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+
+#!/bin/bash
 
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-
-sudo apt install libfuse-dev 
+# 替换高版本golang ）
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 
+echo "✅ diy-part2 路由默认参数修改完成"
